8bb998c7701127cffa142332e8ef6c615a3c4bef
taminhnhat <taminhnhat98@gmail.com>
2021-11-05 10:22:02 +0700

    update multi wall

--- consoleApp.js
+++ consoleApp.js
@@ -55,50 +55,183 @@ let generateConfirmCmd = function (name, wall, row, col, state) {
     return confirmObj;
 };
 
-const returnWall = function () {
+const returnWall = function (wallIndex) {
     const row = Math.floor(Math.random() * 30) + 1;
-    const wallName = 'M-3-' + row;
-    if (wall(wallName).full) return returnWall();
+    const wallName = `M-${wallIndex}-${row}`;
+    if (wall(wallName).full) return returnWall(wallIndex);
     else return wallName;
 }
 
-let M1 = { name: 'M-3-1', count: 0, max: 2, full: false };
-let M2 = { name: 'M-3-2', count: 0, max: 3, full: false };
-let M4 = { name: 'M-3-3', count: 0, max: 2, full: false };
-let M5 = { name: 'M-3-4', count: 0, max: 3, full: false };
-let M6 = { name: 'M-3-5', count: 0, max: 2, full: false };
-let M7 = { name: 'M-3-6', count: 0, max: 3, full: false };
-let M8 = { name: 'M-3-7', count: 0, max: 2, full: false };
-let M9 = { name: 'M-3-8', count: 0, max: 3, full: false };
-let M10 = { name: 'M-3-9', count: 0, max: 2, full: false };
-let M11 = { name: 'M-3-10', count: 0, max: 3, full: false };
-let M12 = { name: 'M-3-11', count: 0, max: 2, full: false };
-let M13 = { name: 'M-3-12', count: 0, max: 3, full: false };
-let M14 = { name: 'M-3-13', count: 0, max: 2, full: false };
-let M15 = { name: 'M-3-14', count: 0, max: 3, full: false };
-let M16 = { name: 'M-3-15', count: 0, max: 2, full: false };
-let M17 = { name: 'M-3-16', count: 0, max: 3, full: false };
-let M18 = { name: 'M-3-17', count: 0, max: 2, full: false };
-let M19 = { name: 'M-3-18', count: 0, max: 3, full: false };
-let M20 = { name: 'M-3-19', count: 0, max: 2, full: false };
-let M21 = { name: 'M-3-20', count: 0, max: 3, full: false };
-let M22 = { name: 'M-3-21', count: 0, max: 2, full: false };
-let M23 = { name: 'M-3-22', count: 0, max: 3, full: false };
-let M24 = { name: 'M-3-23', count: 0, max: 2, full: false };
-let M25 = { name: 'M-3-24', count: 0, max: 3, full: false };
-let M26 = { name: 'M-3-25', count: 0, max: 2, full: false };
-let M27 = { name: 'M-3-26', count: 0, max: 3, full: false };
-let M28 = { name: 'M-3-27', count: 0, max: 2, full: false };
-let M29 = { name: 'M-3-28', count: 0, max: 3, full: false };
-let M30 = { name: 'M-3-29', count: 0, max: 2, full: false };
-let M31 = { name: 'M-3-30', count: 0, max: 3, full: false };
+let M_1_1 = { name: 'M-1-1', count: 0, max: 2, full: false };
+let M_1_2 = { name: 'M-1-2', count: 0, max: 3, full: false };
+let M_1_4 = { name: 'M-1-3', count: 0, max: 2, full: false };
+let M_1_5 = { name: 'M-1-4', count: 0, max: 3, full: false };
+let M_1_6 = { name: 'M-1-5', count: 0, max: 2, full: false };
+let M_1_7 = { name: 'M-1-6', count: 0, max: 3, full: false };
+let M_1_8 = { name: 'M-1-7', count: 0, max: 2, full: false };
+let M_1_9 = { name: 'M-1-8', count: 0, max: 3, full: false };
+let M_1_10 = { name: 'M-1-9', count: 0, max: 2, full: false };
+let M_1_11 = { name: 'M-1-10', count: 0, max: 3, full: false };
+let M_1_12 = { name: 'M-1-11', count: 0, max: 2, full: false };
+let M_1_13 = { name: 'M-1-12', count: 0, max: 3, full: false };
+let M_1_14 = { name: 'M-1-13', count: 0, max: 2, full: false };
+let M_1_15 = { name: 'M-1-14', count: 0, max: 3, full: false };
+let M_1_16 = { name: 'M-1-15', count: 0, max: 2, full: false };
+let M_1_17 = { name: 'M-1-16', count: 0, max: 3, full: false };
+let M_1_18 = { name: 'M-1-17', count: 0, max: 2, full: false };
+let M_1_19 = { name: 'M-1-18', count: 0, max: 3, full: false };
+let M_1_20 = { name: 'M-1-19', count: 0, max: 2, full: false };
+let M_1_21 = { name: 'M-1-20', count: 0, max: 3, full: false };
+let M_1_22 = { name: 'M-1-21', count: 0, max: 2, full: false };
+let M_1_23 = { name: 'M-1-22', count: 0, max: 3, full: false };
+let M_1_24 = { name: 'M-1-23', count: 0, max: 2, full: false };
+let M_1_25 = { name: 'M-1-24', count: 0, max: 3, full: false };
+let M_1_26 = { name: 'M-1-25', count: 0, max: 2, full: false };
+let M_1_27 = { name: 'M-1-26', count: 0, max: 3, full: false };
+let M_1_28 = { name: 'M-1-27', count: 0, max: 2, full: false };
+let M_1_29 = { name: 'M-1-28', count: 0, max: 3, full: false };
+let M_1_30 = { name: 'M-1-29', count: 0, max: 2, full: false };
+let M_1_31 = { name: 'M-1-30', count: 0, max: 3, full: false };
 
-const M = [M1, M2, M4, M5, M6, M7, M8, M9, M10, M11, M12, M13, M14, M15, M16, M17, M18, M19, M20, M21, M22, M23, M24, M25, M26, M27, M28, M29, M30, M31]
+const M1 = [M_1_1, M_1_2, M_1_4, M_1_5, M_1_6, M_1_7, M_1_8, M_1_9, M_1_10, M_1_11, M_1_12, M_1_13, M_1_14, M_1_15, M_1_16, M_1_17, M_1_18, M_1_19, M_1_20, M_1_21, M_1_22, M_1_23, M_1_24, M_1_25, M_1_26, M_1_27, M_1_28, M_1_29, M_1_30, M_1_31];
 
-const wall = function (name) {
-    for (i in M) {
-        if (M[i].name == name) return M[i]
-    }
+let M_2_1 = { name: 'M-2-1', count: 0, max: 2, full: false };
+let M_2_2 = { name: 'M-2-2', count: 0, max: 3, full: false };
+let M_2_4 = { name: 'M-2-3', count: 0, max: 2, full: false };
+let M_2_5 = { name: 'M-2-4', count: 0, max: 3, full: false };
+let M_2_6 = { name: 'M-2-5', count: 0, max: 2, full: false };
+let M_2_7 = { name: 'M-2-6', count: 0, max: 3, full: false };
+let M_2_8 = { name: 'M-2-7', count: 0, max: 2, full: false };
+let M_2_9 = { name: 'M-2-8', count: 0, max: 3, full: false };
+let M_2_10 = { name: 'M-2-9', count: 0, max: 2, full: false };
+let M_2_11 = { name: 'M-2-10', count: 0, max: 3, full: false };
+let M_2_12 = { name: 'M-2-11', count: 0, max: 2, full: false };
+let M_2_13 = { name: 'M-2-12', count: 0, max: 3, full: false };
+let M_2_14 = { name: 'M-2-13', count: 0, max: 2, full: false };
+let M_2_15 = { name: 'M-2-14', count: 0, max: 3, full: false };
+let M_2_16 = { name: 'M-2-15', count: 0, max: 2, full: false };
+let M_2_17 = { name: 'M-2-16', count: 0, max: 3, full: false };
+let M_2_18 = { name: 'M-2-17', count: 0, max: 2, full: false };
+let M_2_19 = { name: 'M-2-18', count: 0, max: 3, full: false };
+let M_2_20 = { name: 'M-2-19', count: 0, max: 2, full: false };
+let M_2_21 = { name: 'M-2-20', count: 0, max: 3, full: false };
+let M_2_22 = { name: 'M-2-21', count: 0, max: 2, full: false };
+let M_2_23 = { name: 'M-2-22', count: 0, max: 3, full: false };
+let M_2_24 = { name: 'M-2-23', count: 0, max: 2, full: false };
+let M_2_25 = { name: 'M-2-24', count: 0, max: 3, full: false };
+let M_2_26 = { name: 'M-2-25', count: 0, max: 2, full: false };
+let M_2_27 = { name: 'M-2-26', count: 0, max: 3, full: false };
+let M_2_28 = { name: 'M-2-27', count: 0, max: 2, full: false };
+let M_2_29 = { name: 'M-2-28', count: 0, max: 3, full: false };
+let M_2_30 = { name: 'M-2-29', count: 0, max: 2, full: false };
+let M_2_31 = { name: 'M-2-30', count: 0, max: 3, full: false };
+
+const M2 = [M_2_1, M_2_2, M_2_4, M_2_5, M_2_6, M_2_7, M_2_8, M_2_9, M_2_10, M_2_11, M_2_12, M_2_13, M_2_14, M_2_15, M_2_16, M_2_17, M_2_18, M_2_19, M_2_20, M_2_21, M_2_22, M_2_23, M_2_24, M_2_25, M_2_26, M_2_27, M_2_28, M_2_29, M_2_30, M_2_31];
+
+let M_3_1 = { name: 'M-3-1', count: 0, max: 2, full: false };
+let M_3_2 = { name: 'M-3-2', count: 0, max: 3, full: false };
+let M_3_4 = { name: 'M-3-3', count: 0, max: 2, full: false };
+let M_3_5 = { name: 'M-3-4', count: 0, max: 3, full: false };
+let M_3_6 = { name: 'M-3-5', count: 0, max: 2, full: false };
+let M_3_7 = { name: 'M-3-6', count: 0, max: 3, full: false };
+let M_3_8 = { name: 'M-3-7', count: 0, max: 2, full: false };
+let M_3_9 = { name: 'M-3-8', count: 0, max: 3, full: false };
+let M_3_10 = { name: 'M-3-9', count: 0, max: 2, full: false };
+let M_3_11 = { name: 'M-3-10', count: 0, max: 3, full: false };
+let M_3_12 = { name: 'M-3-11', count: 0, max: 2, full: false };
+let M_3_13 = { name: 'M-3-12', count: 0, max: 3, full: false };
+let M_3_14 = { name: 'M-3-13', count: 0, max: 2, full: false };
+let M_3_15 = { name: 'M-3-14', count: 0, max: 3, full: false };
+let M_3_16 = { name: 'M-3-15', count: 0, max: 2, full: false };
+let M_3_17 = { name: 'M-3-16', count: 0, max: 3, full: false };
+let M_3_18 = { name: 'M-3-17', count: 0, max: 2, full: false };
+let M_3_19 = { name: 'M-3-18', count: 0, max: 3, full: false };
+let M_3_20 = { name: 'M-3-19', count: 0, max: 2, full: false };
+let M_3_21 = { name: 'M-3-20', count: 0, max: 3, full: false };
+let M_3_22 = { name: 'M-3-21', count: 0, max: 2, full: false };
+let M_3_23 = { name: 'M-3-22', count: 0, max: 3, full: false };
+let M_3_24 = { name: 'M-3-23', count: 0, max: 2, full: false };
+let M_3_25 = { name: 'M-3-24', count: 0, max: 3, full: false };
+let M_3_26 = { name: 'M-3-25', count: 0, max: 2, full: false };
+let M_3_27 = { name: 'M-3-26', count: 0, max: 3, full: false };
+let M_3_28 = { name: 'M-3-27', count: 0, max: 2, full: false };
+let M_3_29 = { name: 'M-3-28', count: 0, max: 3, full: false };
+let M_3_30 = { name: 'M-3-29', count: 0, max: 2, full: false };
+let M_3_31 = { name: 'M-3-30', count: 0, max: 3, full: false };
+
+const M3 = [M_3_1, M_3_2, M_3_4, M_3_5, M_3_6, M_3_7, M_3_8, M_3_9, M_3_10, M_3_11, M_3_12, M_3_13, M_3_14, M_3_15, M_3_16, M_3_17, M_3_18, M_3_19, M_3_20, M_3_21, M_3_22, M_3_23, M_3_24, M_3_25, M_3_26, M_3_27, M_3_28, M_3_29, M_3_30, M_3_31]
+
+let M_4_1 = { name: 'M-4-1', count: 0, max: 2, full: false };
+let M_4_2 = { name: 'M-4-2', count: 0, max: 3, full: false };
+let M_4_4 = { name: 'M-4-3', count: 0, max: 2, full: false };
+let M_4_5 = { name: 'M-4-4', count: 0, max: 3, full: false };
+let M_4_6 = { name: 'M-4-5', count: 0, max: 2, full: false };
+let M_4_7 = { name: 'M-4-6', count: 0, max: 3, full: false };
+let M_4_8 = { name: 'M-4-7', count: 0, max: 2, full: false };
+let M_4_9 = { name: 'M-4-8', count: 0, max: 3, full: false };
+let M_4_10 = { name: 'M-4-9', count: 0, max: 2, full: false };
+let M_4_11 = { name: 'M-4-10', count: 0, max: 3, full: false };
+let M_4_12 = { name: 'M-4-11', count: 0, max: 2, full: false };
+let M_4_13 = { name: 'M-4-12', count: 0, max: 3, full: false };
+let M_4_14 = { name: 'M-4-13', count: 0, max: 2, full: false };
+let M_4_15 = { name: 'M-4-14', count: 0, max: 3, full: false };
+let M_4_16 = { name: 'M-4-15', count: 0, max: 2, full: false };
+
+const M4 = [M_4_1, M_4_2, M_4_4, M_4_5, M_4_6, M_4_7, M_4_8, M_4_9, M_4_10, M_4_11, M_4_12, M_4_13, M_4_14, M_4_15, M_4_16];
+
+// let M1 = { name: 'M-3-1', count: 0, max: 2, full: false };
+// let M2 = { name: 'M-3-2', count: 0, max: 3, full: false };
+// let M4 = { name: 'M-3-3', count: 0, max: 2, full: false };
+// let M5 = { name: 'M-3-4', count: 0, max: 3, full: false };
+// let M6 = { name: 'M-3-5', count: 0, max: 2, full: false };
+// let M7 = { name: 'M-3-6', count: 0, max: 3, full: false };
+// let M8 = { name: 'M-3-7', count: 0, max: 2, full: false };
+// let M9 = { name: 'M-3-8', count: 0, max: 3, full: false };
+// let M10 = { name: 'M-3-9', count: 0, max: 2, full: false };
+// let M11 = { name: 'M-3-10', count: 0, max: 3, full: false };
+// let M12 = { name: 'M-3-11', count: 0, max: 2, full: false };
+// let M13 = { name: 'M-3-12', count: 0, max: 3, full: false };
+// let M14 = { name: 'M-3-13', count: 0, max: 2, full: false };
+// let M15 = { name: 'M-3-14', count: 0, max: 3, full: false };
+// let M16 = { name: 'M-3-15', count: 0, max: 2, full: false };
+// let M17 = { name: 'M-3-16', count: 0, max: 3, full: false };
+// let M18 = { name: 'M-3-17', count: 0, max: 2, full: false };
+// let M19 = { name: 'M-3-18', count: 0, max: 3, full: false };
+// let M20 = { name: 'M-3-19', count: 0, max: 2, full: false };
+// let M21 = { name: 'M-3-20', count: 0, max: 3, full: false };
+// let M22 = { name: 'M-3-21', count: 0, max: 2, full: false };
+// let M23 = { name: 'M-3-22', count: 0, max: 3, full: false };
+// let M24 = { name: 'M-3-23', count: 0, max: 2, full: false };
+// let M25 = { name: 'M-3-24', count: 0, max: 3, full: false };
+// let M26 = { name: 'M-3-25', count: 0, max: 2, full: false };
+// let M27 = { name: 'M-3-26', count: 0, max: 3, full: false };
+// let M28 = { name: 'M-3-27', count: 0, max: 2, full: false };
+// let M29 = { name: 'M-3-28', count: 0, max: 3, full: false };
+// let M30 = { name: 'M-3-29', count: 0, max: 2, full: false };
+// let M31 = { name: 'M-3-30', count: 0, max: 3, full: false };
+
+// const M = [M1, M2, M4, M5, M6, M7, M8, M9, M10, M11, M12, M13, M14, M15, M16, M17, M18, M19, M20, M21, M22, M23, M24, M25, M26, M27, M28, M29, M30, M31]
+
+const wall = function (wallName) {
+    const tmp = wallName.split('-');
+    const wallIndex = tmp[1];
+    if (wallIndex == 1) {
+        for (i in M1) {
+            if (M1[i].name == wallName) return M1[i]
+        }
+    } else if (wallIndex == 2) {
+        for (i in M2) {
+            if (M2[i].name == wallName) return M2[i]
+        }
+    } else if (wallIndex == 3) {
+        for (i in M3) {
+            if (M3[i].name == wallName) return M3[i]
+        }
+    } else if (wallIndex == 4) {
+        for (i in M4) {
+            if (M4[i].name == wallName) return M4[i]
+        }
+    } else return 'error';
 }
 
 console.log('Start listening')
@@ -152,9 +285,9 @@ io.on('connection', function (socket) {
         //     socket.emit('confirmWall', generateConfirmApi(scanApi.key));
 
         if (testEnabled) {
-            let wallName = returnWall();
+            let wallName = returnWall(scanApi.params.wallIndex);
             while (wall(wallName).full == true) {
-                wallName = returnWall();
+                wallName = returnWall(scanApi.params.wallIndex);
             }
             // while(wallName == 'M-3-1' || wallName == 'M-3-7' || wallName =='M-3-13' || wallName == 'M-3-19' || wallName == 'M-3-25'){
             //     wallName = returnWall();
